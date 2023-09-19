744a278107a2f71a86b3a083347c3de1797b9dc3
nergız seyidova <nergisseyidova@gmail.com>
2023-09-19 11:44:55 +0400

    checker comments

--- week3/day1/exerciseXp/exercise3.js
+++ week3/day1/exerciseXp/exercise3.js
@@ -2,3 +2,5 @@ let div1 = document.querySelector('div')
 div1.setAttribute('id','socialNetworkNavigation')
 let li1 = document.createElement('li')
 li1.textContent = 'logout'
+
+// !!! elementi create etdikden sonra mutleq append etmelisen ki DOM trede yerini alsin
