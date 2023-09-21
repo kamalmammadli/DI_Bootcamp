eb018d70ee7a4f8af5027df8d251c73fc8e13513
nergız seyidova <nergisseyidova@gmail.com>
2023-09-21 12:03:23 +0400

    checker comments

--- week3/day2/exerciseXP/exercise1.js
+++ week3/day2/exerciseXP/exercise1.js
@@ -6,14 +6,25 @@ console.log(article);
 let h2 = article.lastElementChild;
 console.log(h2);
 article.removeChild(h2);
+
+let h2HTML = document.getElementsByTagName('h2')[0];
+console.log("🚀 ~ file: exercise1.js:11 ~ h2HTML:", h2HTML)
 let h3 = document.getElementsByTagName('h3')[0];
-h3.addEventListener("click",backgroundToRed);
+console.log("🚀 ~ file: exercise1.js:11 ~ h3:", h3)
+
+
+h2HTML.addEventListener("click",backgroundToRed);
+
 function backgroundToRed(e){
     e.target.style.background ='red';
 }
+
 let h4 = document.getElementsByTagName('h4')[0];
 
 h3.addEventListener("click",hideTheElement);
 function hideTheElement(e){
     e.target.style.display = 'none';
 }
+
+// !!! eyni eventi tek elemente verdikde eventler arasinda konflikt yaranir 
+// !!! meselen click eventi iki funksiyani eyni element uzerinde gore bilmez sonuncu event ise dusecek
