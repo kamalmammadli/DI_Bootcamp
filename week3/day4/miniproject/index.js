e2d63601d78f9485616418e714e7d2761c851c46
nergız seyidova <nergisseyidova@gmail.com>
2023-09-22 12:16:39 +0400

    checker comments on Main branch

--- week3/day4/miniproject/index.js
+++ week3/day4/miniproject/index.js
@@ -5,7 +5,7 @@ let fill_blocks = document.querySelectorAll("#box > *");
 let clear_button = document.getElementsByTagName("button")[0];
 
 clear_button.addEventListener("click", function(){
-    for (fill_block of fill_blocks){
+    for (let fill_block of fill_blocks){  /* !!! let yazilmalidi for loop da yeni variable yaradildigi ucun */
         fill_block.style.backgroundColor = "white";
     }
 });
@@ -19,8 +19,17 @@ body.addEventListener("mouseup", function(){
 })
 
 
-for (color_block of color_blocks){
-    color_block.addEventListener("click", function(event){
-        color = event.target.style.backgroundColor;
-    });
+for (let color_block of color_blocks){
+  /* !!! let yazilmalidi for loop da yeni variable yaradildigi ucun */
+  color_block.addEventListener('click', function (event) {
+    color = event.target.style.backgroundColor;
+  });
 }
+
+// STEPS
+// Step 1 - reng blocklarina add event listener vermek ; hemin reng blockunun rengini goturmek; onu global variable menimsetmek
+// Step 2 - div blocklarina add event listener vermek; mousedown , mouse up , mouse over
+// Step 3 - rengleme function yazmaq; event targete background style vermek; value global variable olacaq
+// step 4 - div blocklarinda rengleme callback function vermek
+
+// !!! TASK-i TAMAMLA
