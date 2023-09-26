04014a5e7f6a337a1fa99883e0ca2c0f44fd856e
kamalmammadli <kamal.memmedli.14@gmail.com>
2023-09-26 14:06:14 +0400

    comment

---
+++ week4/exerciseXp/index.js
@@ -0,0 +1,48 @@
+
+// Exercise 2
+let winBattle = () => {
+    return true
+   }
+   let experiencePoints; 
+   let a =  winBattle()? experiencePoints = 10 : experiencePoints = 1;
+   console.log(experiencePoints);
+   
+   // Exercise 3
+   
+   let isString =(e)=>{
+     if(typeof(e) === 'string'){
+     return true
+     } else {
+       return false
+     }
+   }
+   console.log(isString('hello')); 
+   console.log(isString([1, 2, 4, 0]));
+   
+   
+//    Exercise 4
+   const colors = ["Blue", "Green", "Red", "Orange", "Violet", "Indigo", "Yellow"];
+   
+   let colorsss =()=>{
+     for(let i = 0; i < colors.length; i++){
+       console.log(`${i+1} choice is ${colors[i]}`);
+       if(colors[i] == 'Violet'){
+         console.log('Yeah')
+       } else {
+         console.log('No...')
+       }
+     }
+   }
+   colorsss();
+   console.log('==================');
+   
+   
+   // Exercise 5
+   const colorss = ["Blue", "Green", "Red", "Orange", "Violet", "Indigo", "Yellow"];
+   const ordinal = ["th","st","nd","rd"];
+   
+   colorss.forEach((element, index)=>{
+     index < 3? console.log(`${index+1}${ordinal[index+1]} choice is ${element}`)
+     : console.log(`${index+1}${ordinal[0]} choice is ${element}`)
+     }
+   )
