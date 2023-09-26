c22c69357a037f6cd60c7d717c5dc62f5e5d6d55
kamalmammadli <kamal.memmedli.14@gmail.com>
2023-09-26 23:54:47 +0400

    comment

---
+++ week4/day2/DailyChallange/index.js
@@ -0,0 +1,36 @@
+// const groceries = {
+//     fruits: ["apple", "banana", "cherry"],
+//     totalPrice: "$30",
+//     paid: true,
+//   };
+  
+  
+//   const displayGroceries = () => {
+//     groceries.fruits.slice(0, 3).forEach((fruit) => {
+//       console.log(fruit);
+//     });
+//   };
+  
+  
+//   const cloneGroceries = () => {
+
+//     const shopping = { ...groceries };
+  
+    
+//     const user = shopping; 
+  
+    
+//     shopping.totalPrice = "$35";
+//     shopping.paid = false;
+  
+    
+//     console.log("Modified shopping object:", shopping);
+//     console.log("User object:", user);
+//   };
+  
+  
+//   displayGroceries();
+  
+  
+//   cloneGroceries();
+  
--- week4/day2/exerciseXp/index.js
+++ week4/day2/exerciseXp/index.js
@@ -1,7 +1,46 @@
 // exercise 1
 // İki ədədi parametr kimi qəbul edən və cəmi qaytaran bir sətir funksiyası (yəni ox funksiyası) yaradın.
 
-let add = a => b => a + b;
-let result = add(4)(5)
-console.log(result)
 
+// const num = (a, b) => a + b;
+// console.log(num(4, 5))
+
+
+// exercise 2
+// Kiloqramla çəki alan və onu qramla qaytaran funksiya yaradın. ( İpucu: 1 kq 1000 qr)
+// Əvvəlcə funksiya bəyannaməsindən istifadə edin və onu çağırın.
+// Sonra funksiya ifadəsindən istifadə edin və onu çağırın.
+// Funksiya elanı ilə funksiya ifadəsi arasındakı fərqi bir sətirdə şərh yazın.
+// Nəhayət, bir sətirli ox funksiyasından istifadə edin və onu çağırın.
+
+// function kgToGramsDeclaration(weightInKg) {
+//     return weightInKg * 1000;}
+
+//     console.log(kgToGramsDeclaration(5));
+
+//     const kgToGramsExpression = function(weightInKg) {
+//         return weightInKg * 1000;}
+//         console.log(kgToGramsExpression(5)); 
+
+//         const kgToGramsArrow = weightInKg => weightInKg * 1000;
+//         console.log(kgToGramsArrow(5));
+
+// exercise3
+// (function (numberOfChildren, partnerName, geographicLocation, jobTitle) {
+//     console.log(`I have ${numberOfChildren} children.`);
+//     console.log(`My partner's name is ${partnerName}.`);
+//     console.log(`We live in ${geographicLocation}.`);
+//     console.log(`I work as a ${jobTitle}.`);
+//   })(3, "Kamal", "Baku", "Devloper");
+
+// (function (numberOfChildren, partnerName, geographicLocation, jobTitle) {
+
+//     const sentence = `You will be a ${jobTitle} in ${geographicLocation}, and married to ${partnerName} with ${numberOfChildren} kids.`;
+  
+  
+//     const sentenceElement = document.createElement("p");
+//     sentenceElement.textContent = sentence;
+  
+
+//     document.body.appendChild(sentenceElement);
+//   })(3, "Kamal", "Baku", "Devloper");
