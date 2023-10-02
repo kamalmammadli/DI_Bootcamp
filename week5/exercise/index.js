91431f7adfc4aa5b13a0585eed16b713f1ef2192
kamalmammadli <kamal.memmedli.14@gmail.com>
2023-10-02 15:49:17 +0400

    first commit

---
+++ week5/exercise/index.js
@@ -0,0 +1,26 @@
+const marioGame = {
+    detail : "An amazing game!",
+    characters : {
+        mario : {
+          description:"Small and jumpy. Likes princesses.",
+          height: 10,
+          weight: 3,
+          speed: 12,
+        },
+        bowser : {
+          description: "Big and green, Hates princesses.",
+          height: 16,
+          weight: 6,
+          speed: 4,
+        },
+        princessPeach : {
+          description: "Beautiful princess.",
+          height: 12,
+          weight: 2,
+          speed: 2,
+        }
+    },
+  }
+  const jsonString = JSON.stringify(marioGame, null, 2);
+
+console.log(jsonString);
