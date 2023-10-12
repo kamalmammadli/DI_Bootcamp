1626fc5ed69ef6f3091878e844007c226e1476e1
kamalmammadli <kamal.memmedli.14@gmail.com>
2023-10-12 16:12:15 +0400

    first commit

---
+++ week6/day4/exercisexp/exp.sql
@@ -0,0 +1,135 @@
+-- Database: hr.backup
+
+-- DROP DATABASE IF EXISTS "hr.backup";
+
+-- CREATE DATABASE "hr.backup"
+--     WITH
+--     OWNER = postgres
+--     ENCODING = 'UTF8'
+--     LC_COLLATE = 'Turkish_Turkey.1251'
+--     LC_CTYPE = 'Turkish_Turkey.1251'
+--     TABLESPACE = pg_default
+--     CONNECTION LIMIT = -1
+--     IS_TEMPLATE = False;
+
+
+
+-- select first_name as first, last_name as last from employees;
+
+-- select distinct department_id from employees;
+
+
+
+-- select * from employees order by first_name desc;
+
+
+-- select first_name, last_name, salary * 0.15 as PF from employees;
+
+
+
+-- select first_name, last_name, salary from employees order by salary asc;
+
+-- select sum(salary) as totalsalary from employees;
+
+
+-- SELECT MAX(salary) AS MaximumSalary, MIN(salary) AS MinimumSalary
+-- FROM employees;
+
+-- SELECT AVG(salary) AS AverageSalary
+-- FROM employees;
+
+
+
+-- SELECT COUNT(*) AS NumberOfEmployees
+-- FROM employees;
+
+-- SELECT UPPER(first_name) FROM employees;
+
+-- SELECT SUBSTRING(first_name, 1, 3) FROM employees;
+
+-- SELECT CONCAT(first_name, ' ', last_name) FROM employees;
+
+
+
+
+-- SELECT 
+--     first_name,
+--     last_name,
+--     LENGTH(CONCAT(first_name, ' ', last_name))
+-- FROM 
+--     employees;
+
+
+
+-- SELECT 
+--     first_name
+-- FROM 
+--     employees
+-- WHERE 
+--     first_name'[0-9]';
+
+-- select * from employees limit 10;
+
+-- select first_name, last_name, salary from employees
+-- where salary between 10000 and 15000;
+
+
+-- SELECT *
+-- FROM employees
+-- WHERE first_name LIKE '%c%' AND first_name LIKE '%e%';
+-- select * from employees
+
+-- SELECT last_name, job_title, salary
+-- FROM employees join jobs on employees.job_id=jobs.job_id
+-- WHERE job_title  NOT IN ('Programmer', 'Shipping Clerk')
+-- AND salary NOT IN (4500, 10000, 15000);
+
+-- select * from jobs
+-- select * from employees
+
+-- SELECT last_name
+-- FROM employees
+-- WHERE LENGTH(last_name) = 6;
+
+
+-- SELECT last_name
+-- FROM employees
+-- WHERE SUBSTRING(last_name, 3, 1) = 'e';
+
+
+-- SELECT DISTINCT job_title
+-- FROM employees join jobs on employees.job_id=jobs.job_id
+
+
+
+
+-- select * from employees where UPPER(last_name) in ('JONES','BLAKE','SCOTT','KING','FORD')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	
+	
